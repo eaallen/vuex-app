b5d9f73d79fef24f12077eadf4597da3b411eda4
Elijah Allen <56412012+eaallen@users.noreply.github.com>
2022-05-07 00:48:41 -0600

    made simple vue counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+  <div class="counter">
+    <div class="count">{{ count }}</div>
+    <div class="actions">
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
 </script>
+
+<style scoped>
+button {
+  font-size: 3rem;
+  width: 6rem;
+  height: 4rem;
+  margin: 1rem;
+  border-radius: 5px;
+}
+.count {
+  font-size: 6rem;
+}
+</style>
